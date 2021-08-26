e895158429b208509f0c28cb07ba1066f0e9d1da
Taku Yamamoto <jts0ymt@gmail.com>
2021-08-26 15:46:10 +0900

    activity_main.xmlが完成した

--- app/src/main/java/com/tkymt/catch_the_ball/MainActivity.java
+++ app/src/main/java/com/tkymt/catch_the_ball/MainActivity.java
@@ -3,12 +3,32 @@ package com.tkymt.catch_the_ball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView scoreLabel;
+    private TextView startLabel;
+    private ImageView box;
+    private ImageView orange;
+    private ImageView pink;
+    private ImageView blank;
+
+    // 位置
+    private float boxY;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scoreLabel = findViewById(R.id.scoreLabel);
+        startLabel = findViewById(R.id.startLabel);
+
+        box = findViewById(R.id.box);
+        blank = findViewById(R.id.orange);
+        pink = findViewById(R.id.pink);
     }
 }
