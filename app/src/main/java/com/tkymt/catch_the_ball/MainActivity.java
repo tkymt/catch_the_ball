2798598c81a99119280e7e06ee293207ed431ace
Taku Yamamoto <jts0ymt@gmail.com>
2021-08-26 16:08:08 +0900

    第三回のタッチイベントで画像を動かすが完了した

--- app/src/main/java/com/tkymt/catch_the_ball/MainActivity.java
+++ app/src/main/java/com/tkymt/catch_the_ball/MainActivity.java
@@ -3,6 +3,8 @@ package com.tkymt.catch_the_ball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,8 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView box;
     private ImageView orange;
     private ImageView pink;
-    private ImageView blank;
-
+    private ImageView black;
     // 位置
     private float boxY;
 
@@ -26,9 +27,28 @@ public class MainActivity extends AppCompatActivity {
 
         scoreLabel = findViewById(R.id.scoreLabel);
         startLabel = findViewById(R.id.startLabel);
-
         box = findViewById(R.id.box);
-        blank = findViewById(R.id.orange);
+        orange = findViewById(R.id.orange);
         pink = findViewById(R.id.pink);
+        black = findViewById(R.id.black);
+
+        orange.setX(-80.0f);
+        orange.setY(-80.0f);
+        pink.setX(-80.0f);
+        pink.setY(-80.0f);
+        black.setX(-80.0f);
+        black.setY(-80.0f);
+
+        startLabel.setVisibility(View.INVISIBLE);
+        boxY = 500.0f;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction()==MotionEvent.ACTION_DOWN){
+            boxY -= 20;
+        }
+        box.setY(boxY);
+        return true;
     }
 }
